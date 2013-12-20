5c74d325b67d468534eade1c78c7997858346284
Charlie Mills <charlie@computerbacon.com>
2013-12-20 02:32:05 +0000

    Added a CBV to serve the homepage.

--- potato/blog/views.py
+++ potato/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.base import View, TemplateView
 
-# Create your views here.
+class HomeView(TemplateView):
+    #Todo: Obviously I need to replace this with a paginated class based view
+    template_name = "blog/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        return context
