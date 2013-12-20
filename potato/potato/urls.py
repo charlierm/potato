692fc774445030a3ccf2f01a6887a2f2e8838923
Charlie Mills <charlie@computerbacon.com>
2013-12-20 02:31:30 +0000

    Added application urls.py and included them in the root urls.py file.

--- potato/potato/urls.py
+++ potato/potato/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,7 +9,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'potato.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', include('blog.urls')), #Include blog URLS
     url(r'^admin/', include(admin.site.urls)),
-
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
